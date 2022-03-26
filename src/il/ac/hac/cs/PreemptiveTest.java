5dd16f863a16291444bff14e8ad5d757b5d48fc5
Hezi <yhirshhorn@gmail.com>
2022-03-26 20:36:46 +0300

    Code update

--- src/il/ac/hac/cs/PreemptiveTest.java
+++ src/il/ac/hac/cs/PreemptiveTest.java
@@ -1,27 +1,81 @@
 package il.ac.hac.cs;
 
 public class PreemptiveTest {
+
     public static void main(String[] args) {
 
-        new Thread(() -> {
-           for (int i = 0; i < 5; ++i) {
-               for (int j = 0; j < 50; ++j) {
-                   System.out.print("-");
-               }
-               System.out.println();
-           }
-        }).start();
+        try {
 
-        new Thread(() -> {
-            for (int i = 0; i < 5; ++i) {
-                for (int j = 0; j < 50; ++j) {
-                    System.out.print("*");
-                }
-                System.out.println();
+            System.out.println("--- Starting PrintThread1 ---\n");
+
+            PrintThread1 starlet1 = new PrintThread1("*");
+            PrintThread1 dash1 = new PrintThread1("-");
+            PrintThread1 underscore1 = new PrintThread1("_");
+
+            starlet1.start();
+            dash1.start();
+            underscore1.start();
+
+            // Let the PrintThread1 instances finish execution before executing PrintThread2:
+            starlet1.join();
+            dash1.join();
+            underscore1.join();
+
+            System.out.println("\n--- PrintThread1 is done, starting PrintThread2 ---\n");
+
+            PrintThread2 starlet2 = new PrintThread2("*");
+            PrintThread2 dash2 = new PrintThread2("-");
+            PrintThread2 underscore2 = new PrintThread2("_");
+
+            starlet2.start();
+            dash2.start();
+            underscore2.start();
+
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
+
+class PrintThread1 extends Thread {
+    String name;
+
+    public PrintThread1(String name) {
+        this.name = name;
+    }
+
+    public void run() {
+        printName();
+    }
+
+    private void printName() {
+        for (int i = 1; i < 31; i++) {
+            try {
+                sleep((long) (Math.random() * 100));
+                // or yield()
+            } catch (InterruptedException ie) {
             }
-        }).start();
+            System.out.print(name);
+        }
+    }
+}
 
-        // If your runtime is not preemptive you should see 5 lines of 50 '-' and then 5 lines of 50 '*'.
-        // If your output is mixed then your runtime is preemptive.
+class PrintThread2 extends Thread {
+    String name;
+
+    public PrintThread2(String name) {
+        this.name = name;
+    }
+
+    public void run() {
+        printName();
+    }
+
+    private void printName() {
+        for (int i = 1; i < 31; i++) {
+            System.out.print(name);
+        }
     }
 }
---
+++ src/il/ac/hac/cs/PreemptiveTest2.java
@@ -0,0 +1,27 @@
+package il.ac.hac.cs;
+
+public class PreemptiveTest2 {
+    public static void main(String[] args) {
+
+        new Thread(() -> {
+           for (int i = 0; i < 5; ++i) {
+               for (int j = 0; j < 50; ++j) {
+                   System.out.print("-");
+               }
+               System.out.println();
+           }
+        }).start();
+
+        new Thread(() -> {
+            for (int i = 0; i < 5; ++i) {
+                for (int j = 0; j < 50; ++j) {
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+        }).start();
+
+        // If your runtime is not preemptive you should see 5 lines of 50 '-' and then 5 lines of 50 '*'.
+        // If your output is mixed then your runtime is preemptive.
+    }
+}
